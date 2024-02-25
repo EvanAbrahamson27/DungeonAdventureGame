c09b3c02114a99db094ad64dada3595eaedc2ee5
aaniyahalyes <Aaniyahalyes@gmail.com>
2024-02-24 23:50:40 -0800

    Rebase project

--- src/model/Gremlin.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Gremlin extends Monster {
-    public Gremlin(String theName, int theHealthPoints, int theDamageMin, int theDamageMax, int theAttackSpeed, double theChanceToHit, double theChanceToHeal) {
-        super(theName, theHealthPoints, theDamageMin, theDamageMax, theAttackSpeed, theChanceToHit, theChanceToHeal);
-    }
-}
